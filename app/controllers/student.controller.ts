74ce4972e0ac494ccd48f2d0f1fee9a69c1c9133
Mahdi <mahdihamldar@gmail.com>
2022-12-19 15:24:58 +0330

    CRUD operations endpoint and controllers added

--- app/controllers/student.controller.ts
+++ app/controllers/student.controller.ts
@@ -35,19 +35,121 @@ exports.create = (req: any, res: any) => {
 };
 
 // Retrieve all Students from the database.
-exports.findAll = (req: any, res: any) => {};
+exports.findAll = (req: any, res: any) => {
+  const phoneNumber = req.query.phoneNumber;
+  var condition = phoneNumber
+    ? { phoneNumber: { [Op.like]: `%${phoneNumber}%` } }
+    : null;
+
+  Student.findAll({ where: condition })
+    .then((data: any) => {
+      res.send(data);
+    })
+    .catch((err: any) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving students.",
+      });
+    });
+};
 
 // Find a single Student with an id
-exports.findOne = (req: any, res: any) => {};
+exports.findOne = (req: any, res: any) => {
+  const id = req.params.id;
+
+  Student.findByPk(id)
+    .then((data: any) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Student with id=${id}.`,
+        });
+      }
+    })
+    .catch((err: any) => {
+      res.status(500).send({
+        message: "Error retrieving Student with id=" + id,
+      });
+    });
+};
 
 // Update a Student by the id in the request
-exports.update = (req: any, res: any) => {};
+exports.update = (req: any, res: any) => {
+  const id = req.params.id;
+
+  Student.update(req.body, {
+    where: { id: id },
+  })
+    .then((num: any) => {
+      if (num == 1) {
+        res.send({
+          message: "Student was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update Student with id=${id}. Maybe Student was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err: any) => {
+      res.status(500).send({
+        message: "Error updating Student with id=" + id,
+      });
+    });
+};
 
 // Delete a Student with the specified id in the request
-exports.delete = (req: any, res: any) => {};
+exports.delete = (req: any, res: any) => {
+  const id = req.params.id;
+
+  Student.destroy({
+    where: { id: id },
+  })
+    .then((num: any) => {
+      if (num == 1) {
+        res.send({
+          message: "Student was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Student with id=${id}. Maybe Student was not found!`,
+        });
+      }
+    })
+    .catch((err: any) => {
+      res.status(500).send({
+        message: "Could not delete Student with id=" + id,
+      });
+    });
+};
 
 // Delete all Students from the database.
-exports.deleteAll = (req: any, res: any) => {};
+exports.deleteAll = (req: any, res: any) => {
+  Student.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums: any) => {
+      res.send({ message: `${nums} Students were deleted successfully!` });
+    })
+    .catch((err: any) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Students.",
+      });
+    });
+};
 
-// Find all graduated Students
-exports.findAllPublished = (req: any, res: any) => {};
+exports.findAllGraduated = (req: any, res: any) => {
+  Student.findAll({ where: { graduated: true } })
+    .then((data: any) => {
+      res.send(data);
+    })
+    .catch((err: any) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving students.",
+      });
+    });
+};
--- app/routes/student.routes.ts
+++ app/routes/student.routes.ts
@@ -3,8 +3,26 @@ module.exports = (app: any) => {
 
   var router = require("express").Router();
 
-  // Create a new student
+  // Create a new Student
   router.post("/", students.create);
+
+  // Retrieve all Students
+  router.get("/", students.findAll);
+
+  // Retrieve all graduated Students
+  router.get("/graduated", students.findAllGraduated);
+
+  // Retrieve a single Student with id
+  router.get("/:id", students.findOne);
+
+  // Update a Student with id
+  router.put("/:id", students.update);
+
+  // Delete a Student with id
+  router.delete("/:id", students.delete);
+
+  // Delete all Students
+  router.delete("/", students.deleteAll);
 
   app.use("/api/students", router);
 };
